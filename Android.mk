864edf0fa2f958355c622cb613c3afa61a4e51ab
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0530

    OP4BFB: Make Android.mk search recursive
    Change-Id: I21dc98f0d8187305b200d141f1329b825e17848d
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- Android.mk
+++ Android.mk
@@ -7,6 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP4BFB)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-include $(CLEAR_VARS)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
