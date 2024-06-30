f7a12004d52058b22aae492e2e6ade35f5d6a39c
Emmanuel <alzadidine33@gmail.com>
2024-06-30 08:09:31 +0000

    OP4BFB: Import rootdir scripts from stock
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -10,5 +10,26 @@ LOCAL_PATH := device/oppo/OP4BFB
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)-kernel/Image.gz-dtb:kernel
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6765.rc \
+    init.mt6765.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    multi_init.rc \
+    fstab.mt6765 \
+    ueventd.mt6765.rc
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/oppo/OP4BFB/OP4BFB-vendor.mk)
