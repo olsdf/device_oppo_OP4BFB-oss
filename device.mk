b4b696432f2afc52e832430bc697e42496e48fd8
Emmanuel <alzadidine33@gmail.com>
2024-07-27 14:22:51 +0000

    OP4BFB: Import WiFi configs from stock * from CPH2083PUEX_11.A.67_0670_202201182155
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -61,5 +61,11 @@ PRODUCT_PACKAGES += \
     fstab.mt6765 \
     ueventd.mt6765.rc
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/oppo/OP4BFB/OP4BFB-vendor.mk)
